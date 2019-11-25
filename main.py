30d08e12f79a8ae6ea066f2341c479f5b754f52a
Lasse Benninga <lassebenninga@gmail.com>
2019-11-25 22:42:04 +0100

    feat: run_crawler takes `request` from Google Cloud Function HTTP call

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def run_crawler():
     process = CrawlerProcess(get_project_settings())
     process.crawl('events')
     process.start()  # the script will block here until the crawling is finished
+def run_crawler(request):
+    """
+    Takes a HTTP Cloud Function request and starts the `event` spider.
+    :param request:
+    """
