8aadec7422211a20529aa77253341e1e9ed6a690
Lasse Benninga <lassebenninga@gmail.com>
2019-11-25 22:44:20 +0100

    refactor: Use CrawlerRunner instead of CrawlProcess er In order to re-use the crawler after the spider is done, it is recommended to use `CrawlerRunner`: https://docs.scrapy.org/en/latest/topics/practices.html#run-from-script

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
 
 
-def run_crawler():
-    process = CrawlerProcess(get_project_settings())
-    process.crawl('events')
-    process.start()  # the script will block here until the crawling is finished
 def run_crawler(request):
     """
     Takes a HTTP Cloud Function request and starts the `event` spider.
     :param request:
     """
+    runner = CrawlerRunner(get_project_settings())
+
+    d = runner.crawl('events')
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()  # the script will block here until the crawling is finished
